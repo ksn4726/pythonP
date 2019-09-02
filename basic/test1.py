1704381de1d97f59ddcd2070184395570779d8bb
ksn4726 <ksn4726@naver.com>
2019-09-02 12:52:45 +0900

    20190902 두번째

--- basic/test1.py
+++ basic/test1.py
@@ -1,1 +1,24 @@
 print("hi~")
+a=3
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+s='pyt\nhnon'
+print(s)
+s1="python"
+s2="python's"
+s3='"python"'
+s4='''python'''
+s5="""python"""
+s6="p     y"
+s7='''p
+      y'''
+head="python"
+tail="is fun!!"
+print(head+tail)
+print(type(head))
+print(type(tail))
+
